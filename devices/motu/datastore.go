bf0019de49d00ae249e9f17e50901245a45a24b8
Justin Ginn <Justindavid.ginn@gmail.com>
2024-12-29 23:05:38 +0400

    motu: attach callbacks to changes in datastore

--- devices/motu/datastore.go
+++ devices/motu/datastore.go
@@ -4,10 +4,21 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
-	"io"
 	"net/http"
 	"strconv"
-	"strings"
+)
+
+type allowedTypes interface {
+	int64 | float64 | string | bool
+}
+
+type (
+	EffectInt   func(int64) error
+	EffectFloat func(float64) error
+	EffectStr   func(string) error
+	EffectBool  func(bool) error
+
+	Effect[T allowedTypes] func(T) error
 )
 
 type Datastore interface {
@@ -22,126 +33,66 @@ type Datastore interface {
 	SetStr(key, value string) error
 }
 
-func getAtPath(thisData map[string]any, path string) (any, error) {
-	thisEntry, remaining, ok := strings.Cut(path, "/")
-	if !ok {
-		return thisData[thisEntry], nil
-	}
-	nextData, ok := thisData[thisEntry]
-	if !ok {
-		// TODO: this message can be better
-		return nil, fmt.Errorf("Path element %s in path %s not found", thisEntry, path)
-	}
-	switch nextData := nextData.(type) {
-	case map[string]any:
-		return getAtPath(nextData, remaining)
-	default:
-		return nil, fmt.Errorf("Path element %s in path %s has no children", thisEntry, path)
-	}
-}
-
-func setAtPath(thisData map[string]any, path string, newData any) error {
-	thisEntry, remaining, ok := strings.Cut(path, "/")
-	if !ok {
-		thisData[thisEntry] = newData
-		return nil
-	}
-	// TODO: use reflect to type check here
-	nextData, ok := thisData[thisEntry]
-	if !ok {
-		// TODO: this message can be better
-		return fmt.Errorf("Path element %s in path %s not found", thisEntry, path)
-	}
-	switch nextData := nextData.(type) {
-	case map[string]any:
-		return setAtPath(nextData, remaining, newData)
-	default:
-		return fmt.Errorf("Path element %s in path %s has no children", thisEntry, path)
-	}
-}
-
-type FileDatastore struct {
-	File  io.Reader
-	cache map[string]any
-}
-
-func (d *FileDatastore) Fetch(key string) error {
-	var data map[string]any
-	json.NewDecoder(d.File).Decode(data)
-	for _, entry := range strings.SplitN(key, "/", -1) {
-		newData, ok := data[entry]
-		if !ok {
-			return fmt.Errorf("Could not find %s in %s", entry, key)
-		}
-		return setAtPath(d.cache, key, newData)
-	}
-	return nil
-}
-
-func (d *FileDatastore) GetInt(key string) (int64, error) {
-	val, err := getAtPath(d.cache, key)
-	if err != nil {
-		return 0, err
-	}
-	cast, ok := val.(int64)
-	if !ok {
-		return 0, fmt.Errorf("Type of %s is not int", key)
-	}
-	return cast, nil
-}
-
-func (d *FileDatastore) GetFloat(key string) (float64, error) {
-	val, err := getAtPath(d.cache, key)
-	if err != nil {
-		return 0, err
-	}
-	cast, ok := val.(float64)
-	if !ok {
-		return 0, fmt.Errorf("Type of %s is not float", key)
-	}
-	return cast, nil
-}
-
-func (d *FileDatastore) GetStr(key string) (string, error) {
-	val, err := getAtPath(d.cache, key)
-	if err != nil {
-		return "", err
-	}
-	cast, ok := val.(string)
-	if !ok {
-		return "", fmt.Errorf("Type of %s is not float", key)
-	}
-	return cast, nil
-}
-
-func (d *FileDatastore) SetInt(key string, val int64) error {
-	return setAtPath(d.cache, key, val)
-}
-
-func (d *FileDatastore) SetFloat(key string, val float64) error {
-	return setAtPath(d.cache, key, val)
-}
-
-func (d *FileDatastore) SetStr(key string, val string) error {
-	return setAtPath(d.cache, key, val)
-}
-
 type HTTPDatastore struct {
 	Client http.Client
 	url    string
 	cache  map[string]any
+
+	actionInt   map[string]Effect[int64]
+	actionFloat map[string]Effect[float64]
+	actionStr   map[string]Effect[string]
+	actionBool  map[string]Effect[bool]
 }
 
 func NewHTTPDatastore(url string) HTTPDatastore {
 	return HTTPDatastore{
-		Client: http.Client{},
-		url:    url,
-		cache:  make(map[string]any),
+		Client:      http.Client{},
+		url:         url,
+		cache:       make(map[string]any),
+		actionInt:   map[string]Effect[int64]{},
+		actionFloat: map[string]Effect[float64]{},
+		actionStr:   map[string]Effect[string]{},
+		actionBool:  map[string]Effect[bool]{},
 	}
 }
 
+func (d *HTTPDatastore) RegisterIntEffect(key string, e Effect[int64]) {
+	d.actionInt[key] = e
+}
+
+func (d *HTTPDatastore) RegisterFloatEffect(key string, e Effect[float64]) {
+	d.actionFloat[key] = e
+}
+
+func (d *HTTPDatastore) RegisterStrEffect(key string, e Effect[string]) {
+	d.actionStr[key] = e
+}
+
+func handleEffect[T allowedTypes](effects map[string]Effect[T], oldData, newData map[string]any) error {
+	for k, e := range effects {
+		if new, inNew := newData[k]; inNew {
+			old, inOld := oldData[k]
+			if !inOld {
+				// TODO: this could panic; handle gracefully
+				if err := e(new.(T)); err != nil {
+					// TODO: handle this gracefully
+					panic(err)
+				}
+			}
+			if old != new {
+				if err := e(new.(T)); err != nil {
+					panic(err)
+				}
+			}
+		}
+	}
+	return nil
+}
+
+// TODO: set up some kind of polling hooks for changed values
 func (d *HTTPDatastore) poll() {
 	etag := 0
+	newData := map[string]any{}
 	for {
 		req, err := http.NewRequest(http.MethodGet, d.url, nil)
 		req.Header.Set("If-None-Match", fmt.Sprintf("%d", etag))
@@ -161,14 +112,87 @@ func (d *HTTPDatastore) poll() {
 			if err != nil {
 				panic(err)
 			}
-			if err := json.NewDecoder(resp.Body).Decode(&d.cache); err != nil {
+			if err := json.NewDecoder(resp.Body).Decode(&newData); err != nil {
 				panic(err)
 			}
+			if err := handleEffect[int64](d.actionInt, d.cache, newData); err != nil {
+				panic(err)
+			}
+			// Check for changes in any entries that have an effect registered
+			// Don't bother checking the rest
+			for k, e := range d.actionInt {
+				if new, inNew := newData[k]; inNew {
+					old, inOld := d.cache[k]
+					if !inOld {
+						// TODO: this could panic; handle gracefully
+						if err := e(new.(int64)); err != nil {
+							// TODO: handle this gracefully
+							panic(err)
+						}
+					}
+					if old != new {
+						if err := e(new.(int64)); err != nil {
+							panic(err)
+						}
+					}
+				}
+			}
+			for k, e := range d.actionFloat {
+				if new, inNew := newData[k]; inNew {
+					old, inOld := d.cache[k]
+					if !inOld {
+						// TODO: this could panic; handle gracefully
+						if err := e(new.(float64)); err != nil {
+							// TODO: handle this gracefully
+							panic(err)
+						}
+					}
+					if old != new {
+						if err := e(new.(float64)); err != nil {
+							panic(err)
+						}
+					}
+				}
+			}
+			for k, e := range d.actionStr {
+				if new, inNew := newData[k]; inNew {
+					old, inOld := d.cache[k]
+					if !inOld {
+						// TODO: this could panic; handle gracefully
+						if err := e(new.(string)); err != nil {
+							// TODO: handle this gracefully
+							panic(err)
+						}
+					}
+					if old != new {
+						if err := e(new.(string)); err != nil {
+							panic(err)
+						}
+					}
+				}
+			}
+			for k, e := range d.actionBool {
+				if new, inNew := newData[k]; inNew {
+					old, inOld := d.cache[k]
+					if !inOld {
+						// TODO: this could panic; handle gracefully
+						if err := e(new.(bool)); err != nil {
+							// TODO: handle this gracefully
+							panic(err)
+						}
+					}
+					if old != new {
+						if err := e(new.(bool)); err != nil {
+							panic(err)
+						}
+					}
+				}
+			}
+			d.cache = newData
 		}
 	}
 }
 
-// TODO: do this with proper struct types
 func (d *HTTPDatastore) GetInt(key string) (int64, error) {
 	val, ok := d.cache[key]
 	if !ok {
@@ -194,22 +218,31 @@ func (d *HTTPDatastore) GetInt(key string) (int64, error) {
 }
 
 func (d *HTTPDatastore) GetFloat(key string) (float64, error) {
-	val, err := getAtPath(d.cache, key)
-	if err != nil {
-		return 0, err
-	}
-	cast, ok := val.(float64)
+	val, ok := d.cache[key]
 	if !ok {
-		return 0, fmt.Errorf("Type of %s is not float", key)
+		return 0, fmt.Errorf("Could not find %s", key)
 	}
-	return cast, nil
+	switch val := val.(type) {
+	case int64:
+		return float64(val), nil
+	case int:
+		return float64(val), nil
+	case float32:
+		return float64(val), nil
+	case float64:
+		return float64(val), nil
+	case string:
+		cast, err := strconv.Atoi(val)
+		if err != nil {
+			return 0, fmt.Errorf("Cannot cast %s to int", key)
+		}
+		return float64(cast), nil
+	}
+	panic(fmt.Sprintf("Unsupported type %T", val))
 }
 
 func (d *HTTPDatastore) GetStr(key string) (string, error) {
-	val, err := getAtPath(d.cache, key)
-	if err != nil {
-		return "", err
-	}
+	val, ok := d.cache[key]
 	cast, ok := val.(string)
 	if !ok {
 		return "", fmt.Errorf("Type of %s is not string", key)
@@ -217,10 +250,110 @@ func (d *HTTPDatastore) GetStr(key string) (string, error) {
 	return cast, nil
 }
 
+func (d *HTTPDatastore) GetBool(key string) (bool, error) {
+	val, ok := d.cache[key]
+	cast, ok := val.(bool)
+	if !ok {
+		return false, fmt.Errorf("Type of %s is not string", key)
+	}
+	return cast, nil
+}
+
 func (d *HTTPDatastore) SetInt(key string, value int64) error {
 	d.cache[key] = value
 
+	// if effect, ok := d.actionInt[key]; ok {
+	// 	if err := effect(value); err != nil {
+	// 		// TODO: log this instead of returning
+	// 		return err
+	// 	}
+	// }
+
 	jsonData := []byte(fmt.Sprintf(`json={"%s":"%d"}`, key, value))
+
+	req, err := http.NewRequest(http.MethodPatch, d.url, bytes.NewBuffer(jsonData))
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	req.Header.Add("Accept-Encoding", "*/*")
+	if err != nil {
+		return err
+	}
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	defer resp.Body.Close()
+	if resp.StatusCode != 200 {
+		panic(resp.StatusCode)
+	}
+
+	return nil
+}
+
+func (d *HTTPDatastore) SetFloat(key string, value float64) error {
+	d.cache[key] = value
+
+	// if effect, ok := d.actionFloat[key]; ok {
+	// 	if err := effect(value); err != nil {
+	// 		// TODO: log this instead of returning
+	// 		return err
+	// 	}
+	// }
+
+	jsonData := []byte(fmt.Sprintf(`json={"%s":"%f"}`, key, value))
+
+	req, err := http.NewRequest(http.MethodPatch, d.url, bytes.NewBuffer(jsonData))
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	req.Header.Add("Accept-Encoding", "*/*")
+	if err != nil {
+		return err
+	}
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	defer resp.Body.Close()
+	if resp.StatusCode != 200 {
+		panic(resp.StatusCode)
+	}
+
+	return nil
+}
+
+func (d *HTTPDatastore) SetString(key string, value string) error {
+	d.cache[key] = value
+
+	// if effect, ok := d.actionStr[key]; ok {
+	// 	if err := effect(value); err != nil {
+	// 		// TODO: log this instead of returning
+	// 		return err
+	// 	}
+	// }
+
+	jsonData := []byte(fmt.Sprintf(`json={"%s":"%s"}`, key, value))
+
+	req, err := http.NewRequest(http.MethodPatch, d.url, bytes.NewBuffer(jsonData))
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	req.Header.Add("Accept-Encoding", "*/*")
+	if err != nil {
+		return err
+	}
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	defer resp.Body.Close()
+	if resp.StatusCode != 200 {
+		panic(resp.StatusCode)
+	}
+
+	return nil
+}
+
+func (d *HTTPDatastore) SetBool(key string, value bool) error {
+	d.cache[key] = value
+
+	// if effect, ok := d.actionBool[key]; ok {
+	// 	if err := effect(value); err != nil {
+	// 		// TODO: log this instead of returning
+	// 		return err
+	// 	}
+	// }
+
+	jsonData := []byte(fmt.Sprintf(`json={"%s":"%s"}`, key, value))
 
 	req, err := http.NewRequest(http.MethodPatch, d.url, bytes.NewBuffer(jsonData))
 	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
