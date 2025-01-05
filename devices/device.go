7bdf527c63ebf10c7aeeff1d81282fc4e56638b4
Justin Ginn <Justindavid.ginn@gmail.com>
2025-01-05 21:37:26 +0400

    devices: add godoc

--- devices/device.go
+++ devices/device.go
@@ -1,9 +1,11 @@
 package devices
 
+// BaseTypes is the set of types we can create effects for
 type BaseTypes interface {
 	int64 | float64 | string | bool
 }
 
-type (
-	Effect[T BaseTypes] func(T) error
-)
+// Effect[T] defines a callback function that operates on an object of type T and returns an error.
+//
+// Effects are registered by a device.
+type Effect[T BaseTypes] func(T) error
--- devices/midi.go
+++ devices/midi.go
@@ -7,6 +7,7 @@ import (
 	"gitlab.com/gomidi/midi/v2/drivers"
 )
 
+// EffectCC defines a callback function that operates as a result of a control change message.
 type EffectCC func(uint8) error
 
 type actionCC struct {
@@ -15,6 +16,7 @@ type actionCC struct {
 	action     EffectCC
 }
 
+// EffectPitchBend defines a callback function that operates as a result of a pitch bend message.
 type EffectPitchBend func(int16, uint16) error
 
 type actionPitchBend struct {
@@ -22,6 +24,7 @@ type actionPitchBend struct {
 	action  EffectPitchBend
 }
 
+// EffectNote defines a callback function that operates as a result of a note message.
 type EffectNote func(bool) error
 
 type actionNote struct {
@@ -30,13 +33,15 @@ type actionNote struct {
 	action  EffectNote
 }
 
+// EffectNote defines a callback function that operates as a result of an AfterTouch message.
+type EffectAftertouch func(uint8) error
+
 type actionAfterTouch struct {
 	channel uint8
 	action  EffectAftertouch
 }
 
-type EffectAftertouch func(uint8) error
-
+// MidiDevice represents a generic MIDI device and allows registering effects for various messages the device may receive.
 type MidiDevice struct {
 	inPort  drivers.In
 	outPort drivers.Out
@@ -58,6 +63,7 @@ func NewMidiDevice(inPort drivers.In, outPort drivers.Out) MidiDevice {
 	}
 }
 
+// RegisterNote specifies an action that should be run each time a control change message is received for the specifeid channel and controller.
 func (f *MidiDevice) RegisterCC(channel, controller uint8, action EffectCC) {
 	f.cc = append(f.cc, actionCC{
 		channel:    channel,
@@ -66,6 +72,7 @@ func (f *MidiDevice) RegisterCC(channel, controller uint8, action EffectCC) {
 	})
 }
 
+// RegisterNote specifies an action that should be run each time a note message is received for the specifeid channel and key.
 func (f *MidiDevice) RegisterNote(channel, key uint8, action EffectNote) {
 	f.note = append(f.note, actionNote{
 		channel: channel,
@@ -74,6 +81,7 @@ func (f *MidiDevice) RegisterNote(channel, key uint8, action EffectNote) {
 	})
 }
 
+// RegisterPitchBend specifies an action that should be run each time a pitchbend message is received for the specified channel.
 func (f *MidiDevice) RegisterPitchBend(channel uint8, action EffectPitchBend) {
 	f.pitchBend = append(f.pitchBend, actionPitchBend{
 		channel: channel,
@@ -81,34 +89,40 @@ func (f *MidiDevice) RegisterPitchBend(channel uint8, action EffectPitchBend) {
 	})
 }
 
+// RegisterPitchBend specifies an action that should be run each time a channel pressure message is received for the specified channel.
 func (f *MidiDevice) RegisterChannelPressure(channel uint8, action EffectAftertouch) {
 }
 
+// Send sends a message to this device's outPort.
 func (f *MidiDevice) Send(msg midi.Message) error {
 	return f.outPort.Send(msg)
 }
 
+// Run starts this device and causes it to listen and respond to incoming MIDI messages.
+//
+// For any message with an effect registered, that effect will be run each time such a message is received.
 func (f *MidiDevice) Run() {
 	defer midi.CloseDriver()
 
-	// TODO: wtf
 	in, err := midi.FindInPort("VMPK")
 	if err != nil {
-		fmt.Println("can't find VMPK")
+		fmt.Println("can't find VMPK:", err)
 		return
 	}
 
-	stop, err := midi.ListenTo(in, func(msg midi.Message, timestampms int32) {
+	var stop func()
+	stop, err = midi.ListenTo(in, func(msg midi.Message, timestampms int32) {
 		switch msg.Type() {
 		case midi.ControlChangeMsg:
 			var channel, control, value uint8
 			if ok := msg.GetControlChange(&channel, &control, &value); !ok {
-				panic("bad")
+				fmt.Println("failed to parse Control Change message:", err)
+				return
 			}
 			for _, action := range f.cc {
 				if action.channel == channel && action.controller == control {
 					if err := action.action(value); err != nil {
-						panic("bad")
+						fmt.Println("failed to process Control Change:", err)
 					}
 					break
 				}
@@ -118,12 +132,13 @@ func (f *MidiDevice) Run() {
 			var relative int16
 			var absolute uint16
 			if ok := msg.GetPitchBend(&channel, &relative, &absolute); !ok {
-				panic("bad")
+				fmt.Println("failed to parse Pitch Bend message:", err)
+				return
 			}
 			for _, action := range f.pitchBend {
 				if action.channel == channel {
 					if err := action.action(relative, absolute); err != nil {
-						panic("bad")
+						fmt.Println("failed to process Pitch Bend:", err)
 					}
 					break
 				}
@@ -131,36 +146,39 @@ func (f *MidiDevice) Run() {
 		case midi.NoteOnMsg:
 			var channel, key, velocity uint8
 			if ok := msg.GetNoteOn(&channel, &key, &velocity); !ok {
-				panic("bad")
+				fmt.Println("failed to parse Note On message:", err)
+				return
 			}
 			for _, action := range f.note {
 				if action.key == key {
 					if err := action.action(true); err != nil {
-						panic("bad")
+						fmt.Println("failed to process Note On:", err)
 					}
 				}
 			}
 		case midi.NoteOffMsg:
 			var channel, key, velocity uint8
 			if ok := msg.GetNoteOff(&channel, &key, &velocity); !ok {
-				panic("bad")
+				fmt.Println("failed to parse Note Off message:", err)
+				return
 			}
 			for _, action := range f.note {
 				if action.key == key {
 					if err := action.action(false); err != nil {
-						panic("bad")
+						fmt.Println("failed to process Note Off:", err)
 					}
 				}
 			}
 		case midi.AfterTouchMsg:
 			var channel, pressure uint8
 			if ok := msg.GetAfterTouch(&channel, &pressure); !ok {
-				panic("bad")
+				fmt.Println("failed to parse After Touch message:", err)
+				return
 			}
 			for _, action := range f.aftertouch {
 				if action.channel == channel {
 					if err := action.action(pressure); err != nil {
-						panic("bad")
+						fmt.Println("failed to process After Touch:", err)
 					}
 				}
 			}
