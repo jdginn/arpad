b554bc5701740e2ffb2b97f4cb910cc39764c734
Justin Ginn <Justindavid.ginn@gmail.com>
2025-05-23 12:49:44 +0200

    Move mode management into its own package
    Protect the spooky internals from main

--- main.go
+++ main.go
@@ -11,8 +11,37 @@ import (
 	motulib "github.com/jdginn/arpad/devices/motu"
 	reaperlib "github.com/jdginn/arpad/devices/reaper"
 	xtouchlib "github.com/jdginn/arpad/devices/xtouch"
+	mLib "github.com/jdginn/arpad/mode"
 )
 
+// Modes:
+//
+// Mix: baseline mode for interacting with the DAW. Primarily mimics traditional MCU features. Supports submodes (triggered by "Flip" button based on context):
+// -> This track sends on fader
+// -> This send all input tracks
+//
+//	In this mode, the 9th fader controls the level for this send
+//
+// In Mix mode, encoders are mapped to pan by default. Push in to trim gain.
+// -> Marker mode (while holding marker button)
+// Lists all markers on the scribble strips and allows jumping to marker by clicking in the encoder
+//
+// Record: baseline mode for interacting with the audio interface's internal mixer. Attempts to mimic the behavior of a large-format analog recording console. Supports submodes (triggered by "Flip" button based on context):
+// -> This track sends to fader (separately label effect auxes vs. outputs)
+// -> This output all input tracks
+// -> This aux all input tracks
+// In record mode, encoders are mapped to gain by default. Push in to control pan.
+//
+// # Timecode display lists the current mode using ascii-to-7seg characters
+//
+// # Mode selection is mapped to Encoder Assign
+//
+// The following buttons are active in every mode:
+// - Modify, Utility, Automation, Transport buttons and jog wheel control the DAW at all times.
+// - Talkback (mapped to Display button)
+// - Global mute (mapped to Global View)
+// - Control room monitoring selection (main monitors, mono mixcube, nearfield monitors, headphones-only, other?) mapped to View bttons (excluding Global View)
+// - Per-channel record arm always controls the DAW
 type Mode uint64
 
 const (
@@ -27,154 +56,23 @@ const (
 	ALL = 0xFFFFFFFFFFFFFFFF
 )
 
-// Modes:
-//
-// Mix: baseline mode for interacting with the DAW. Primarily mimics traditional MCU features. Supports submodes (triggered by "Flip" button based on context):
-// -> This track sends on fader
-// -> This send all input tracks
-// 		In this mode, the 9th fader controls the level for this send
-// In Mix mode, encoders are mapped to pan by default. Push in to trim gain.
-// -> Marker mode (while holding marker button)
-// Lists all markers on the scribble strips and allows jumping to marker by clicking in the encoder
-//
-// Record: baseline mode for interacting with the audio interface's internal mixer. Attempts to mimic the behavior of a large-format analog recording console. Supports submodes (triggered by "Flip" button based on context):
-// -> This track sends to fader (separately label effect auxes vs. outputs)
-// -> This output all input tracks
-// -> This aux all input tracks
-// In record mode, encoders are mapped to gain by default. Push in to control pan.
-//
-// Timecode display lists the current mode using ascii-to-7seg characters
-//
-// Mode selection is mapped to Encoder Assign
-//
-// The following buttons are active in every mode:
-// - Modify, Utility, Automation, Transport buttons and jog wheel control the DAW at all times.
-// - Talkback (mapped to Display button)
-// - Global mute (mapped to Global View)
-// - Control room monitoring selection (main monitors, mono mixcube, nearfield monitors, headphones-only, other?) mapped to View bttons (excluding Global View)
-// - Per-channel record arm always controls the DAW
-
-// event holds a collection of events that should all update the same underlying value.
-//
-// The value is cached.
-type event struct {
-	value   any
-	actions []func(any) error
-}
-
-// layerObserver is a bundle of all the elements registered for a particular mode
-//
-// Each element is referenced by a descriptive string name.
-type layerObserver struct {
-	internal map[any]event
-}
-
-func newLayer() layerObserver {
-	return layerObserver{
-		internal: map[any]event{},
-	}
-}
-
-// ModeManager sets the current mode and manages which effects are active depending on the active mode.
-//
-// Effects of elements in the currently active mode take immediate effect.
-//
-// All inactive modes' effects will not be run until that mode is activated. ModeManager maintains a copy of the value of each element so that the effects
-// can be applied with the correct value immediately when the mode is activated.
 type ModeManager struct {
-	currMode Mode
-	// For updating devices when we switch modes
-	//
-	// For now at least, we are YOLOing any bitwise set of modes as a key and then checking if the current mode is a subset of the key
-	// when we need to update the layer
-	modes map[Mode]layerObserver
+	*mLib.ModeManager[Mode]
 
+	// Extra metadata we want to keep track of that is not actually part of mode management logic
 	selectedTrackMix    string
 	selectedTrackRecord int
-}
-
-func NewModeManager() *ModeManager {
-	return &ModeManager{
-		currMode: MIX,
-		// These modes are hand-written since the list of modes does not change often.
-		modes: map[Mode]layerObserver{
-			MIX:                         newLayer(),
-			MIX_THIS_TRACK_SENDS:        newLayer(),
-			MIX_SENDS_TO_THIS_BUS:       newLayer(),
-			RECORD:                      newLayer(),
-			RECORD_THIS_TRACK_SENDS:     newLayer(),
-			RECORD_SENDS_TO_THIS_AUX:    newLayer(),
-			RECORD_SENDS_TO_THIS_OUTPUT: newLayer(),
-		},
-	}
-}
-
-// SetMode sets the currently active mode.
-//
-// If the new mode is not the same as the current mode, run each effect of each element with its cached value.
-func (c *ModeManager) SetMode(mode Mode) {
-	if c.currMode == mode {
-		return
-	}
-	c.currMode = mode
-	// Run any actions associated with this mode to update devices to match
-	// values stored for this mode while we were in a different mode
-	if _, ok := c.modes[mode]; !ok {
-		c.modes[mode] = newLayer()
-	}
-
-	for m, l := range c.modes {
-		if m|c.currMode != 0 {
-			for _, e := range l.internal {
-				for _, a := range e.actions {
-					a(e.value)
-				}
-			}
-		}
-	}
-}
-
-// Bind binds the callback to the binding site and adds a guard to ensure that the callback is only called for the specified mode
-//
-// The most important thing about this function is that defines its generic types from the types in the passed binder function.
-// This function will come from the device we are binding to, and it is that device's responsibility to tell us the types it expects.
-// Once you have provided the bind function, the language server knows the types you need to provide for the path and callback. This ensures type-safety
-// and gives the language server maximum latitude to help you write a valid callback.
-//
-// There are some functional shenanigans to support proper binding to the device and proper automatic callbacks on mode change within the mode manager.
-// Note that this function doesn't care about the types of the bind path or args to the callback as long as the bind function accepts that pair.
-func Bind[P, A any](mm *ModeManager, mode Mode, binder func(P, func(A) error), path P, callback func(A) error) {
-	// First, we need to tell the mode manager to call the callback with the cached value any time we toggle to this mode
-	elem := mm.modes[mode].internal[path]
-	// We need to type-delete V because it allows us to store all of these actions in one slice (nifty!)
-	elem.actions = append(elem.actions, func(v any) error {
-		// Undo the type deletion here in this closure
-		// Now everything is type-safe again
-		castV := v.(A)
-		return callback(castV)
-	})
-
-	// Now bind the callback to the device, but first wrap it in another closure with guards to only run this for the specified mode.
-	binder(
-		path,
-		func(args A) error {
-			if mm.currMode|mode != 0 {
-				return callback(args)
-			}
-			return nil
-		},
-	)
 }
 
 var mm *ModeManager
 
 func init() {
-	mm = NewModeManager()
+	mm = &ModeManager{mLib.NewModeManager[Mode](MIX), "", 0}
 }
 
 // This is just eliding the first argument because it never changes and visual clarity is at a premium
 func bind[P, A any](mode Mode, binder func(P, func(A) error), path P, callback func(A) error) {
-	Bind(mm, mode, binder, path, callback)
+	mLib.Bind(mm.ModeManager, mode, binder, path, callback)
 }
 
 func main() {
@@ -197,8 +95,6 @@ func main() {
 	motu := motulib.NewHTTPDatastore("http://localhost:8888")
 
 	reaper := reaperlib.OscServer{}
-
-	m := NewModeManager()
 
 	for trackNum := 0; trackNum < 8; trackNum++ {
 		c := xtouch.Channels[trackNum]
@@ -242,13 +138,13 @@ func main() {
 
 		// Select
 		bind(MIX, c.Select.Bind, nil, func(b bool) error {
-			if m.selectedTrackMix, err = motu.GetStr("channels/%d/name"); err != nil {
+			if mm.selectedTrackMix, err = motu.GetStr("channels/%d/name"); err != nil {
 				return err
 			}
 			return nil
 		})
 		bind(RECORD, c.Select.Bind, nil, func(b bool) error {
-			m.selectedTrackRecord = trackNum
+			mm.selectedTrackRecord = trackNum
 			return c.Select.SetLED(xtouchlib.ON)
 		})
 		// TODO: bind incoming select from DAW
---
+++ mode/modemanager.go
@@ -0,0 +1,107 @@
+package mode
+
+import "golang.org/x/exp/constraints"
+
+// event holds a collection of events that should all update the same underlying value.
+//
+// The value is cached.
+type event struct {
+	value   any
+	actions []func(any) error
+}
+
+// layerObserver is a bundle of all the elements registered for a particular mode
+//
+// Each element is referenced by a descriptive string name.
+type layerObserver struct {
+	internal map[any]event
+}
+
+func newLayer() layerObserver {
+	return layerObserver{
+		internal: map[any]event{},
+	}
+}
+
+// ModeManager sets the current mode and manages which effects are active depending on the active mode.
+//
+// Effects of elements in the currently active mode take immediate effect.
+//
+// All inactive modes' effects will not be run until that mode is activated. ModeManager maintains a copy of the value of each element so that the effects
+// can be applied with the correct value immediately when the mode is activated.
+type ModeManager[M constraints.Integer] struct {
+	currMode M
+	// For updating devices when we switch modes
+	//
+	// For now at least, we are YOLOing any bitwise set of modes as a key and then checking if the current mode is a subset of the key
+	// when we need to update the layer
+	modes map[M]layerObserver
+
+	selectedTrackMix    string
+	selectedTrackRecord int
+}
+
+func NewModeManager[M constraints.Integer](startingMode M) *ModeManager[M] {
+	return &ModeManager[M]{
+		currMode: startingMode,
+		// These modes are hand-written since the list of modes does not change often.
+		modes: map[M]layerObserver{},
+	}
+}
+
+// SetMode sets the currently active mode.
+//
+// If the new mode is not the same as the current mode, run each effect of each element with its cached value.
+func (c *ModeManager[M]) SetMode(mode M) {
+	if c.currMode == mode {
+		return
+	}
+	c.currMode = mode
+	// Run any actions associated with this mode to update devices to match
+	// values stored for this mode while we were in a different mode
+	if _, ok := c.modes[mode]; !ok {
+		c.modes[mode] = newLayer()
+	}
+
+	for m, l := range c.modes {
+		if m|c.currMode != 0 {
+			for _, e := range l.internal {
+				for _, a := range e.actions {
+					a(e.value)
+				}
+			}
+		}
+	}
+}
+
+// Bind binds the callback to the binding site and adds a guard to ensure that the callback is only called for the specified mode
+//
+// The most important thing about this function is that defines its generic types from the types in the passed binder function.
+// This function will come from the device we are binding to, and it is that device's responsibility to tell us the types it expects.
+// Once you have provided the bind function, the language server knows the types you need to provide for the path and callback. This ensures type-safety
+// and gives the language server maximum latitude to help you write a valid callback.
+//
+// There are some functional shenanigans to support proper binding to the device and proper automatic callbacks on mode change within the mode manager.
+// Note that this function doesn't care about the types of the bind path or args to the callback as long as the bind function accepts that pair.
+func Bind[P, A any, M constraints.Integer](mm *ModeManager[M], mode M, binder func(P, func(A) error), path P, callback func(A) error) {
+	// First, we need to tell the mode manager to call the callback with the cached value any time we toggle to this mode
+	elem := mm.modes[mode].internal[path]
+	// We need to type-delete V because it allows us to store all of these actions in one slice (nifty!)
+	elem.actions = append(elem.actions, func(v any) error {
+		// Undo the type deletion here in this closure
+		// Now everything is type-safe again
+		castV := v.(A)
+		return callback(castV)
+	})
+
+	// Now bind the callback to the device, but first wrap it in another closure with guards to only run this for the specified mode.
+	binder(
+		path,
+		func(args A) error {
+			if mm.currMode|mode != 0 {
+				return callback(args)
+			}
+			return nil
+		},
+	)
+}
