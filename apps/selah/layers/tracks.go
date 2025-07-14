91cbc16b417f86a0289c790550a7f9f682e5cf55
Justin Ginn <Justindavid.ginn@gmail.com>
2025-07-14 13:11:30 +0200

    apps/selah/tracks: support multiple modes
    Instead of using multiple Binds to express the various modes, give each
    hardware element a single Bind and then switch its behavior based on
    mode.
    
    Various other fixes. Mute/Solo/Rec are working between xtouch and
    Reaper. Faders seem to be mostly working.

--- apps/selah/layers/tracks.go
+++ apps/selah/layers/tracks.go
@@ -14,7 +14,7 @@ import (
 
 const (
 	FADER_EPSILON float64 = 0.001
-	NUM_CHANNELS  int     = 8
+	NUM_CHANNELS  int64   = 8
 )
 
 // get returns the first element in the slice for which the predicate returns true.
@@ -29,31 +29,143 @@ func get[T any](s []T, predicate func(T) bool) (T, bool) {
 	return zero, false
 }
 
+type trackMapping struct {
+	reaperIdx  int64
+	surfaceIdx int64
+}
+
 type TrackManager struct {
 	x             *xtouchlib.XTouchDefault
 	r             *reaper.Reaper
-	tracks        []*TrackData
+	logicalTracks []*TrackData
+	trackMappings map[int64]int64
 	selectedTrack *TrackData
 }
 
+func (m *TrackManager) getTrack(surfaceIdx int64) (*TrackData, bool) {
+	reaperIdx, ok := m.trackMappings[surfaceIdx]
+	if !ok {
+		return &TrackData{}, false
+	}
+	trackData, ok := get(m.logicalTracks, func(track *TrackData) bool {
+		return track.reaperIdx == reaperIdx
+	})
+	if !ok {
+		return &TrackData{}, false
+	}
+	return trackData, true
+}
+
+func (m *TrackManager) AddHardwareTrack(idx int64) {
+	// REC
+	m.x.Channels[idx].Rec.On.Bind(func() error {
+		if t, ok := m.getTrack(idx); ok {
+			switch CurrMode() {
+			case MIX:
+				t.rec = !t.rec
+				return m.r.Track(t.reaperIdx).Recarm.Set(t.rec)
+			}
+		}
+		return nil
+	})
+	// SOLO
+	m.x.Channels[idx].Solo.On.Bind(func() error {
+		if t, ok := m.getTrack(idx); ok {
+			switch CurrMode() {
+			case MIX:
+				t.solo = !t.solo
+				return m.r.Track(t.reaperIdx).Solo.Set(t.solo)
+			}
+		}
+		return nil
+	})
+	// MUTE
+	m.x.Channels[idx].Mute.On.Bind(func() error {
+		if t, ok := m.getTrack(idx); ok {
+			switch CurrMode() {
+			case MIX:
+				t.mute = !t.mute
+				return m.r.Track(t.reaperIdx).Mute.Set(t.mute)
+			}
+		}
+		return nil
+	})
+	// Fader
+	m.x.Channels[idx].Fader.Bind(func(v int16) error {
+		if t, ok := m.getTrack(idx); ok {
+			switch CurrMode() {
+			case MIX:
+				newVal := int16ToNormFloat(v)
+				// Because both feedback and input are implemented on the same physical control for fader,
+				// we need some deduplication to avoid jittering the faders or flooding the system with
+				// echoing messages.
+				if math.Abs(newVal-t.volume) < FADER_EPSILON {
+					t.volume = newVal
+					return nil
+				}
+				t.volume = newVal
+				err := m.r.Track(t.reaperIdx).Volume.Set(t.volume)
+				m.x.Channels[idx].Fader.Set(v)
+				if err != nil {
+					panic(err)
+				}
+				return err
+			case MIX_SELECTED_TRACK_SENDS:
+				// TODO: this indexing is wrong; we need to find send idx from path wildcard values
+				newVal := int16ToNormFloat(v)
+				// Because both feedback and input are implemented on the same physical control for fader,
+				// we need some deduplication to avoid jittering the faders or flooding the system with
+				// echoing messages.
+				if math.Abs(newVal-t.sends[idx].volume) < FADER_EPSILON {
+					t.sends[idx].volume = newVal
+					return nil
+				}
+				t.sends[idx].volume = newVal
+				return m.r.Track(t.reaperIdx).Send(idx).Volume.Set(t.volume)
+			}
+		}
+		return nil
+	})
+	// Pan
+	m.x.Channels[idx].Encoder.Bind(func(v uint8) error {
+		if t, ok := m.getTrack(idx); ok {
+			switch CurrMode() {
+			case MIX:
+				t.pan = float64(v) / float64(math.MaxUint8)
+				return m.r.Track(t.reaperIdx).Pan.Set(t.pan)
+			case MIX_SELECTED_TRACK_SENDS:
+				newVal := float64(v) / float64(math.MaxUint8)
+				// Because both feedback and input are implemented on the same physical control for fader,
+				// we need some deduplication to avoid jittering the faders or flooding the system with
+				// echoing messages.
+				t.pan = newVal
+				return m.r.Track(t.reaperIdx).Send(idx).Pan.Set(t.volume)
+			}
+		}
+		return nil
+	})
+}
+
 func (t *TrackManager) listenForNewTracks() {
-	// TODO: don't panic here
 	// Find and populate our collection of track states
+	//
+	// TODO: don't panic here
 	if err := t.r.OscDispatcher().AddMsgHandler("/track/@/*", func(m *osc.Message) {
 		// TODO: this seems a bit brittle
 		idx, err := strconv.ParseInt(m.Arguments[1].(string), 10, 64)
 		if err != nil {
 			return
 		}
-		if _, exists := get(t.tracks, func(track *TrackData) bool {
+		if _, exists := get(t.logicalTracks, func(track *TrackData) bool {
 			return track.reaperIdx == idx
 		}); !exists {
-			t.tracks = append(t.tracks, NewTrackData(
+			t.logicalTracks = append(t.logicalTracks, NewTrackData(
 				t.x,
 				t.r,
 				idx,
-				idx,
+				idx-1,
 			))
+			t.trackMappings[idx-1] = idx
 		}
 	}); err != nil {
 		panic(err)
@@ -68,17 +180,17 @@ func (t *TrackManager) listenForNewTracks() {
 		if err != nil {
 			return
 		}
-		if _, exists := get(t.tracks, func(track *TrackData) bool {
+		if _, exists := get(t.logicalTracks, func(track *TrackData) bool {
 			return track.reaperIdx == trackIdx
 		}); !exists {
-			t.tracks = append(t.tracks, NewTrackData(
+			t.logicalTracks = append(t.logicalTracks, NewTrackData(
 				t.x,
 				t.r,
 				trackIdx,
-				trackIdx,
+				trackIdx-1,
 			))
 		}
-		track, _ := get(t.tracks, func(track *TrackData) bool {
+		track, _ := get(t.logicalTracks, func(track *TrackData) bool {
 			return track.reaperIdx == trackIdx
 		})
 		track.sends[sendIdx] = NewTrackSendData(track, sendIdx, 0)
@@ -91,7 +203,7 @@ func (t *TrackManager) listenForNewTracks() {
 		if err != nil {
 			return
 		}
-		if selectedTarck, ok := get(t.tracks, func(track *TrackData) bool {
+		if selectedTarck, ok := get(t.logicalTracks, func(track *TrackData) bool {
 			return track.reaperIdx == trackIdx
 		}); ok {
 			t.selectedTrack = selectedTarck
@@ -105,16 +217,20 @@ func (t *TrackManager) listenForNewTracks() {
 
 func NewTrackManager(x *xtouchlib.XTouchDefault, r *reaper.Reaper) *TrackManager {
 	t := &TrackManager{
-		x:      x,
-		r:      r,
-		tracks: make([]*TrackData, 0),
+		x:             x,
+		r:             r,
+		logicalTracks: make([]*TrackData, 0),
+		trackMappings: make(map[int64]int64),
+	}
+	for i := int64(0); i < NUM_CHANNELS; i++ {
+		t.AddHardwareTrack(i)
 	}
 	t.listenForNewTracks()
 	return t
 }
 
 func (m *TrackManager) TransitionMix() (errs error) {
-	for _, track := range m.tracks {
+	for _, track := range m.logicalTracks {
 		errs = errors.Join(errs, track.TransitionMix())
 	}
 	return errs
@@ -156,55 +272,47 @@ func NewTrackData(x *xtouchlib.XTouchDefault, r *reaper.Reaper, reaperIdx, surfa
 	// REC
 	t.r.Track(t.reaperIdx).Recarm.Bind(func(v bool) error {
 		t.rec = v
-		return t.x.Channels[t.surfaceIdx].Solo.LED.Set(v)
-	})
-	FilterBind(MIX, t.x.Channels[t.surfaceIdx].Rec.On).Bind(func(v uint8) error {
-		t.rec = !t.rec
-		return t.r.Track(t.reaperIdx).Recarm.Set(t.rec)
+		switch CurrMode() {
+		case MIX:
+			return t.x.Channels[t.surfaceIdx].Rec.LED.Set(v)
+		}
+		return nil
 	})
 	// SOLO
 	t.r.Track(t.reaperIdx).Solo.Bind(func(v bool) error {
 		t.solo = v
-		return t.x.Channels[t.surfaceIdx].Solo.LED.Set(v)
-	})
-	FilterBind(MIX, t.x.Channels[t.surfaceIdx].Solo.On).Bind(func(v uint8) error {
-		t.solo = !t.solo
-		return t.r.Track(t.reaperIdx).Solo.Set(t.solo)
+		switch CurrMode() {
+		case MIX:
+			return t.x.Channels[t.surfaceIdx].Solo.LED.Set(v)
+		}
+		return nil
 	})
 	// MUTE
 	t.r.Track(t.reaperIdx).Mute.Bind(func(v bool) error {
 		t.mute = v
-		return t.x.Channels[t.surfaceIdx].Mute.LED.Set(v)
-	})
-	FilterBind(MIX, t.x.Channels[t.surfaceIdx].Mute.On).Bind(func(v uint8) error {
-		t.mute = !t.mute
-		return t.r.Track(t.reaperIdx).Mute.Set(t.rec)
+		switch CurrMode() {
+		case MIX:
+			return t.x.Channels[t.surfaceIdx].Mute.LED.Set(v)
+		}
+		return nil
 	})
 	// Fader
 	t.r.Track(t.reaperIdx).Volume.Bind(func(v float64) error {
 		t.volume = v
-		return t.x.Channels[t.surfaceIdx].Fader.Set(normFloatToInt(v))
-	})
-	FilterBind(MIX, t.x.Channels[t.surfaceIdx].Fader).Bind(func(v int16) error {
-		newVal := int16ToNormFloat(v)
-		// Because both feedback and input are implemented on the same physical control for fader,
-		// we need some deduplication to avoid jittering the faders or flooding the system with
-		// echoing messages.
-		if math.Abs(newVal-t.volume) < FADER_EPSILON {
-			t.volume = newVal
-			return nil
+		switch CurrMode() {
+		case MIX:
+			return t.x.Channels[t.surfaceIdx].Fader.Set(normFloatToInt(v))
 		}
-		t.volume = newVal
-		return t.r.Track(t.reaperIdx).Volume.Set(t.volume)
+		return nil
 	})
 	// Pan
 	t.r.Track(t.reaperIdx).Pan.Bind(func(v float64) error {
 		t.pan = v
-		return t.x.Channels[t.surfaceIdx].Encoder.Ring.Set(v) // TODO: verify
-	})
-	FilterBind(MIX, t.x.Channels[t.surfaceIdx].Encoder.Ring).Bind(func(v uint8) error {
-		t.pan = float64(v) / float64(math.MaxUint8)
-		return t.r.Track(t.reaperIdx).Pan.Set(t.pan)
+		switch CurrMode() {
+		case MIX:
+			return t.x.Channels[t.surfaceIdx].Encoder.Ring.Set(v) // TODO: verify
+		}
+		return nil
 	})
 	OnTransition(MIX, t.TransitionMix)
 	return t
